e2bf1df872d4ea8a583f8f14952dba4b7392eea5
JuliaRinzilotti <rintz@posteo.de>
2021-10-26 18:00:12 +0200

    Version_1

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+
+function ball1() {
+    art1.style.background = "linear-gradient(50deg, rgba(23, 46, 218) 50%, rgba(235, 208, 7) 50%)"
+    body.style.background = "rgba(233, 4, 4)";
+    h1.style.color = "rgba(174, 167, 167)";
+}
+
+function ball2() {
+    art2.style.background = "linear-gradient(50deg, rgb(115, 4, 138)50%, rgba(233, 4, 4) 50%)"
+    body.style.background = "rgb(5, 134, 238)";
+    h1.style.color = "rgb(2, 133, 2)";
+}
+
+function ball3() {
+    art3.style.background = "linear-gradient(50deg, rgb(210, 131, 14)50%, rgba(23, 46, 218) 50%)"
+    body.style.background = "rgb(2, 133, 2)";
+    h1.style.color = "rgba(235, 208, 7)";
+}
+
+function ball4() {
+    art4.style.background = "linear-gradient(50deg, rgb(5, 134, 238)50%, rgba(174, 167, 167) 50%)"
+    body.style.background = "rgb(210, 131, 14)";
+    h1.style.color = "rgb(115, 4, 138)";
+}
+
+
+
